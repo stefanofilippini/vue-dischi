c2c4eb36304c1be81e12f4f001fd47106db27a72
stefanofilippini <filippini.stefano96@gmail.com>
2021-11-29 17:19:16 +0100

    added filter

--- src/components/Discs.vue
+++ src/components/Discs.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="container">
+    <div>
+        <SelectNotDinamic @genre="FilterGenre"/>
         <ul class="mt-2">
-            <li class="mt-2" v-for="(element, i) in discogrphi.response" :key="`key-${i}`">
+            <li class="mt-2" v-for="(element, i) in filteredDiscographi" :key="`key-${i}`">
                 <img :src="element.poster" alt="">
                 <h1> {{ element.title }} </h1>
                 <h2> {{ element.author }} </h2>
                 <h2> {{ element.year }} </h2>
                 <h3> {{ element.genre }} </h3>
-
             </li>
         </ul>
+
     </div>
 </template>
 
 <script>
 
 import Axios from 'axios';
+import SelectNotDinamic from '@/components/SelectNotDinamic';
 
 
 export default {
@@ -23,12 +25,30 @@ export default {
 
     data() {
         return {
-        discogrphi: []
+        discogrphi: [],
+        actualGenre: "",
         }
+    },
+
+    
+    components: {
+        SelectNotDinamic,
     },
 
     created() {
         this.call_api()
+    },
+
+    computed: {
+        filteredDiscographi() {
+            if (this.actualGenre === "") {
+                return this.discogrphi.response
+            } else {
+                return this.discogrphi.response.filter((item) => {
+                    return item.genre === this.actualGenre
+                })
+            }
+        }
     },
 
     methods: {
@@ -36,7 +56,11 @@ export default {
             Axios.get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(result => this.discogrphi = result.data)
             .catch(error => console.log(error));
-        }
+        },
+
+        FilterGenre(genre) {
+            this.actualGenre = genre
+        },
     },
     
 }
---
+++ src/components/SelectNotDinamic.vue
@@ -0,0 +1,25 @@
+<template>
+  <select v-model="genreSelect" @change="$emit('genre', genreSelect)">
+      <option value=""></option>
+      <option value="Rock">Rock</option>
+      <option value="Pop">Pop</option>
+      <option value="Jazz">Jazz</option>
+      <option value="Metal">Metal</option>
+  </select>
+</template>
+
+<script>
+export default {
+    name: 'SelectNotDinamic',
+    data() {
+        return {
+            genreSelect: "",
+        }
+    }
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
